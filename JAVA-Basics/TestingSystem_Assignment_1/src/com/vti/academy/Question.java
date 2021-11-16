b062154967e44e8e5f23f4efeacd1fde48b00ad2
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-11-16 17:03:09 +0700

    Create Question.java

---
+++ JAVA-Basics/TestingSystem_Assignment_1/src/com/vti/academy/Question.java
@@ -0,0 +1,13 @@
+package com.vti.academy;
+
+import java.util.Date;
+
+public class Question {
+	byte questionId;
+	String content;
+	CategoryQuestion categoryQuestion;
+	TypeQuestion typeQuestion;
+	Account account;
+	Date createDate;
+	
+}
