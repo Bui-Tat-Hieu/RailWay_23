e0bc876835286334c8bfcff7262aeea08e7626a6
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-11-16 17:02:48 +0700

    Create Account.java

---
+++ JAVA-Basics/TestingSystem_Assignment_1/src/com/vti/academy/Account.java
@@ -0,0 +1,14 @@
+package com.vti.academy;
+
+import java.util.Date;
+
+public class Account {
+	short accountId;
+	String email;
+	String userName;
+	String fullName;
+	Department department;
+	Position position;
+	Date createDate;
+
+}
