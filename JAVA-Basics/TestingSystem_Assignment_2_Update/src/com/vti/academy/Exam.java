6fdcb9d20c4f816b70d0188facfdb2b3864ebcfa
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-11-26 21:52:13 +0700

    Create Exam.java

---
+++ JAVA-Basics/TestingSystem_Assignment_2_Update/src/com/vti/academy/Exam.java
@@ -0,0 +1,13 @@
+package com.vti.academy;
+
+import java.util.Date;
+
+public class Exam {
+	byte examId;
+	String code;
+	String title;
+	CategoryQuestion categoryQuestion;
+	String duration;
+	Account account;
+	Date createDate;
+}
