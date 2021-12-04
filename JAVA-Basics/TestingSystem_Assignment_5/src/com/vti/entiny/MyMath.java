c61850500e5b5f77af223c39d0b068a99a62fa35
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-04 10:20:56 +0700

    Create MyMath.java

---
+++ JAVA-Basics/TestingSystem_Assignment_5/src/com/vti/entiny/MyMath.java
@@ -0,0 +1,39 @@
+package com.vti.entiny;
+
+public class MyMath {
+	
+//	private int diem1;
+//	private byte diem2;
+//	
+//
+	public MyMath() {
+		super();
+	}
+//
+//	public MyMath(int diem1, int diem2) {
+//		super();
+//		this.setDiem1(diem1);
+//		this.setDiem2(diem2);
+//	}
+	
+	public float sum(int diem1, float diem2) {
+		return diem1 + (float)diem2;
+	}
+//
+//	int getDiem1() {
+//		return diem1;
+//	}
+//
+//	public void setDiem1(int diem1) {
+//		this.diem1 = diem1;
+//	}
+//
+//	int getDiem2() {
+//		return diem2;
+//	}
+//
+//	public void setDiem2(int diem2) {
+//		this.diem2 = diem2;
+//	}
+
+}
