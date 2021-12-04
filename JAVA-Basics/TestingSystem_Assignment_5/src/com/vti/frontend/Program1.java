28dcf6e938b02079f8d5b21f288e6eb7a263a91f
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-04 10:21:03 +0700

    Create Program1.java

---
+++ JAVA-Basics/TestingSystem_Assignment_5/src/com/vti/frontend/Program1.java
@@ -0,0 +1,16 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise1_Abstraction;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		Exercise1_Abstraction exe1 = new Exercise1_Abstraction();
+		
+		exe1.question1();
+		
+		
+	}
+
+}
