b41da6e2d40d022d37cb918a08255b6cb8c371e3
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-04 10:21:04 +0700

    Create Program2.java

---
+++ JAVA-Basics/TestingSystem_Assignment_5/src/com/vti/frontend/Program2.java
@@ -0,0 +1,21 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise2_Polymorphism;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Exercise2_Polymorphism exe2 = new Exercise2_Polymorphism();
+		
+//		exe2.question1();
+//		exe2.question3();
+		exe2.question4();
+		
+//		Demo demo = new Demo();
+//		
+//		demo.question11();
+//		
+	}
+
+}
