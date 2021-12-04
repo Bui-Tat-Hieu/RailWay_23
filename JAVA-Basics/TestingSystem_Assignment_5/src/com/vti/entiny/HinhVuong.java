d185daf59e53c30956771a7c1a4576a352277679
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-04 10:20:50 +0700

    Create HinhVuong.java

---
+++ JAVA-Basics/TestingSystem_Assignment_5/src/com/vti/entiny/HinhVuong.java
@@ -0,0 +1,21 @@
+package com.vti.entiny;
+
+public class HinhVuong extends HinhChuNhat {
+
+	public HinhVuong(double dai) {
+		super(dai, dai);
+	}
+	
+	public double tinhDienTich() {
+		return super.tinhDienTich();
+	}
+	
+	public double tinhChuVi() {
+		return super.tinhChuVi();
+	}
+	
+//	public void test() {
+//		System.out.println("Tính theo hình vuông");
+//	}
+
+}
