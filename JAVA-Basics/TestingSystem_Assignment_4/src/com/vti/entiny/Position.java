0e4615e3b3f33af27acb148accd49e9a534c0d2f
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:15:06 +0700

    Create Position.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/entiny/Position.java
@@ -0,0 +1,25 @@
+package com.vti.entiny;
+
+public class Position {
+	private byte positionId;
+	private String positionName;
+	
+	
+	public byte getPositionId() {
+		return positionId;
+	}
+	public void setPositionId(int i) {
+		this.positionId = (byte) i;
+	}
+	
+	
+	public String getPositionName() {
+		return positionName;
+	}
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+	
+	
+
+}
