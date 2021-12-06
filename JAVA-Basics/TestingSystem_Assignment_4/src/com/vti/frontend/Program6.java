1507588db562effbd00cc37845c722c0c9cec075
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:15:16 +0700

    Create Program6.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/frontend/Program6.java
@@ -0,0 +1,14 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise6_Abstraction;
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		
+		Exercise6_Abstraction exe6 = new Exercise6_Abstraction();
+		
+		exe6.question1();
+	}
+
+}
