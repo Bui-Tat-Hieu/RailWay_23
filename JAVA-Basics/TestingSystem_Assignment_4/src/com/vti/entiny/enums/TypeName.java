a2e09fcdabe73f9e641826ca1e39619f9e68e228
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:14:57 +0700

    Create TypeName.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/entiny/enums/TypeName.java
@@ -0,0 +1,18 @@
+package com.vti.entiny.enums;
+
+public enum TypeName {
+	TYPE_NAME_ESSAY("Essay"), 
+	TYPE_NAME_MULTIPLE_CHOICE("Multiple-Choice");
+	
+
+	private String typeName;
+
+	TypeName(String typeName) {
+		this.typeName = typeName;
+	}
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+}
