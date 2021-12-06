c93d7e198306bf0ef20836e5310c5a11f0144147
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:15:15 +0700

    Create Program5.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/frontend/Program5.java
@@ -0,0 +1,16 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise5_Inheritance;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		
+		Exercise5_Inheritance exe5 = new Exercise5_Inheritance();
+		
+		exe5.question1();
+		
+
+	}
+
+}
