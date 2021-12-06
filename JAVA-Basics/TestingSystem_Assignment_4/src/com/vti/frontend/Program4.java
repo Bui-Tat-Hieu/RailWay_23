d98dd99699c58fce229f54d6b22db780deb6e90d
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:15:13 +0700

    Create Program4.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/frontend/Program4.java
@@ -0,0 +1,16 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise4_Encapsulation;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		
+		Exercise4_Encapsulation exe4 = new Exercise4_Encapsulation();
+		
+		exe4.question1();
+		
+
+	}
+
+}
