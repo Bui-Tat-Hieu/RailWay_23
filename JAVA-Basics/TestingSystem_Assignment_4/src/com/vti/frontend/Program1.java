077b07b3f388f722945703874d412e7ce38464eb
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-06 22:15:11 +0700

    Create Program1.java

---
+++ JAVA-Basics/TestingSystem_Assignment_4/src/com/vti/frontend/Program1.java
@@ -0,0 +1,22 @@
+package com.vti.frontend;
+
+import java.text.ParseException;
+
+import com.vti.backend.Exercise1_Constructor;
+
+public class Program1 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Exercise1_Constructor exe1 = new Exercise1_Constructor();
+		
+//		exe1.question1();
+		
+//		exe1.question2();
+		
+		exe1.question3();
+		
+
+	}
+
+}
