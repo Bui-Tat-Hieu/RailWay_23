31019e7fc42335a98ab4669822a38eb7fdbbdbb7
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-09 16:19:05 +0700

    Create Program2.java

---
+++ JAVA-Basics/TestingSystem_Assignment_6/src/com/vti/frontend/Program2.java
@@ -0,0 +1,28 @@
+package com.vti.frontend;
+
+import java.util.Scanner;
+
+import com.vti.backend.Exercise2_Exception;
+
+public class Program2 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(System.in);
+
+		Exercise2_Exception exe2 = new Exercise2_Exception();
+		
+//		exe2.question1();
+//		exe2.question2();
+//		exe2.question3();
+//		exe2.question4(2);
+//		exe2.question5();
+//		exe2.question7();
+//		exe2.question8();
+//		exe2.question9();
+		exe2.question10();
+		
+		scanner.close();
+	}
+
+}
