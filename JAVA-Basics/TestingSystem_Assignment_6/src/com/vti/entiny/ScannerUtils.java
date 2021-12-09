d6ad9af95ec5b10f2d279beaa43291cb7df52a88
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-09 16:19:01 +0700

    Create ScannerUtils.java

---
+++ JAVA-Basics/TestingSystem_Assignment_6/src/com/vti/entiny/ScannerUtils.java
@@ -0,0 +1,72 @@
+package com.vti.entiny;
+
+import java.util.Scanner;
+
+public class ScannerUtils {
+	
+	public Scanner scanner;
+
+	public ScannerUtils(Scanner scanner) {
+		super();
+		this.scanner = scanner;
+	}
+	
+	public int inputInt(String errorMessage) {
+		int output = 0;
+		boolean isOk = false;
+		while (isOk == false) {
+			try {
+				isOk = true;
+				String input = scanner.nextLine();
+				output = Integer.parseInt(input);
+				
+			} catch (NumberFormatException e) {
+				isOk = false;
+				System.out.println(errorMessage);
+			}
+		}
+		return output;
+	}
+	
+	public float inputFloat(String errorMessage) {
+		float output = 0;
+		boolean isOk = false;
+		while (isOk == false) {
+			try {
+				isOk = true;
+				String input = scanner.nextLine();
+				output = Float.valueOf(input);
+				
+			} catch (NumberFormatException e) {
+				isOk = false;
+				System.out.println(errorMessage);
+			}
+		}
+		return output;
+	}
+	
+	public double inputDouble(String errorMessage) {
+		double output = 0;
+		boolean isOk = false;
+		while (isOk == false) {
+			try {
+				isOk = true;
+				String input = scanner.nextLine();
+				output = Double.valueOf(input);
+				
+			} catch (NumberFormatException e) {
+				isOk = false;
+				System.out.println(errorMessage);
+			}
+		}
+		return output;
+	}
+	
+	public String inputString() {
+		
+		return scanner.nextLine();
+	
+	}
+	
+
+}
