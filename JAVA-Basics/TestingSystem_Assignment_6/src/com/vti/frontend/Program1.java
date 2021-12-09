473d7f9ed0e68fc20b9dfed2296bdc07d539fd08
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-09 16:19:03 +0700

    Create Program1.java

---
+++ JAVA-Basics/TestingSystem_Assignment_6/src/com/vti/frontend/Program1.java
@@ -0,0 +1,15 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise1_Debug;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		Exercise1_Debug exe1 = new Exercise1_Debug();
+				
+		exe1.question1();
+		
+	}
+
+}
