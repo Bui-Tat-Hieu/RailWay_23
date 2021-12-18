5ebd3c2a80783d2f6b2379cc8e97d1fb41b887cd
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-18 23:26:57 +0700

    Create Student.java

---
+++ JAVA-Basics/TestingSystem_Assignment_8/src/com/vti/entiny/Student.java
@@ -0,0 +1,46 @@
+package com.vti.entiny;
+
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private static int count;
+
+	
+
+	
+	public Student(String name) {
+		super();
+		count++;
+		this.id = count;
+		this.name = name;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id= " + id 
+				+ ", name= " + name + "]";
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+}
