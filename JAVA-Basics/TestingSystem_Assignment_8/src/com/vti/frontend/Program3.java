7f7fd910be5e778f9d3ed6165f75217ea56c7665
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-18 23:27:02 +0700

    Create Program3.java

---
+++ JAVA-Basics/TestingSystem_Assignment_8/src/com/vti/frontend/Program3.java
@@ -0,0 +1,20 @@
+package com.vti.frontend;
+
+import com.vti.backend.Exercise3_Generic;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+
+		Exercise3_Generic exercise3 = new Exercise3_Generic();
+	
+//		exercise3.question1_2();
+//		exercise3.question4();
+//		exercise3.question5();
+//		exercise3.question6();
+//		exercise3.question7();
+		exercise3.question8();
+		
+	}
+
+}
