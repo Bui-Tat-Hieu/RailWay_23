cb0931dfcc9608dfd4bc21f4d1c29fdef2d946dc
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-18 23:26:51 +0700

    Create MyMap.java

---
+++ JAVA-Basics/TestingSystem_Assignment_8/src/com/vti/entiny/MyMap.java
@@ -0,0 +1,36 @@
+package com.vti.entiny;
+
+public class MyMap <K, V>{
+	
+	K key;
+	V value;
+	
+	public MyMap(K key, V value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "MyMap [key=" + key + ", value=" + value + "]";
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+
+}
