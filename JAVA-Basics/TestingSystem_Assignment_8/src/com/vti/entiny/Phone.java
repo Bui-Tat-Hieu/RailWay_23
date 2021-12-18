b13dc224f789e9e9290b0103ed8c8d0f28be1fb1
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-18 23:26:52 +0700

    Create Phone.java

---
+++ JAVA-Basics/TestingSystem_Assignment_8/src/com/vti/entiny/Phone.java
@@ -0,0 +1,21 @@
+package com.vti.entiny;
+
+public class Phone <K, V> extends MyMap <K, V> {
+
+
+	public Phone(K key, V value) {
+		super(key, value);
+		
+	}
+
+	public String getPhoneNumber() {
+		return (String) super.getValue();
+	}
+
+	public K getKey() {
+		return super.getKey();
+	}
+
+	
+
+}
