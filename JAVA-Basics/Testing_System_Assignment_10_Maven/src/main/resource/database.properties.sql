9cf5991424cda25b0dfd9e03de6e0f4eb6a8bc30
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2021-12-30 14:54:51 +0700

    Create database.properties.sql

---
+++ JAVA-Basics/Testing_System_Assignment_10_Maven/src/main/resource/database.properties.sql
@@ -0,0 +1,4 @@
+url = jdbc:mysql://localhost:3306/testing_system?useUnicode=true&characterEncoding=utf8&autoReconnect=true&useSSL=false
+userName = root
+password = pinocchio
+driverName = com.mysql.cj.jdbc.Driver
