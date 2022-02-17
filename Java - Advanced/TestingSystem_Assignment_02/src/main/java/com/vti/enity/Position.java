668b11094c97e8691379d03bc59560100b8c6a4f
Bui-Tat-Hieu <vitsieupham17895@gmail.com>
2022-02-17 16:31:33 +0700

    Create Position.java

---
+++ Java - Advanced/TestingSystem_Assignment_02/src/main/java/com/vti/enity/Position.java
@@ -0,0 +1,88 @@
+package com.vti.enity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.vti.enity.enums.PositionName;
+import com.vti.enity.enums.PositionNameConverter;
+
+@Entity
+@Table(name = "`position`")
+public class Position  implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "position_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private short id;
+	
+	@Column(name = "`position_name`", nullable = false, unique = true)
+	@Convert(converter = PositionNameConverter.class)
+	private PositionName positionName;
+
+	public Position() {
+		super();
+	}
+
+	public short getId() {
+		return id;
+	}
+
+	public void setId(short id) {
+		this.id = id;
+	}
+
+	public PositionName getPositionName() {
+		return positionName;
+	}
+
+	public void setPositionName(PositionName positionName) {
+		this.positionName = positionName;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [id=" + id + ", positionName=" + positionName + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
